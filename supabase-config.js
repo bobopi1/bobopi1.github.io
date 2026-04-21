a7a884931eb074ecf75f40cf00f304d1eef0bbf6
William Davidson <117697713+bobopi1@users.noreply.github.com>
2026-04-21 14:07:06 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.BUDGETFLOW_SUPABASE_URL = "https://bobopi1.github.io/";
+window.BUDGETFLOW_SUPABASE_URL = "https://oqtlajbnrueyhvbfhbds.supabase.co";
 window.BUDGETFLOW_SUPABASE_KEY = "sb_publishable_1GFj5Ot92qnkBNqgTFdc0w_1nefzLB8";
