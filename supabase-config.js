1f970c38d3715159442d2eac6864904b696f1bfe
William Davidson <117697713+bobopi1@users.noreply.github.com>
2026-04-21 13:34:40 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.BUDGETFLOW_SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-window.BUDGETFLOW_SUPABASE_KEY = "YOUR_PUBLIC_PUBLISHABLE_OR_ANON_KEY";
+window.BUDGETFLOW_SUPABASE_URL = "https://bobopi1.github.io/";
+window.BUDGETFLOW_SUPABASE_KEY = "sb_publishable_1GFj5Ot92qnkBNqgTFdc0w_1nefzLB8";
